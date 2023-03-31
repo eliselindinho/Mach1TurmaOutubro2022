b315901bf4dae33fa4e9d3f194a96de68564f47e
eliselindinho <eliselindinho@gmail.com>
2023-03-31 14:15:41 -0300

    Inserindo o enunciado das atividades práticas

--- JavaScript/JavaScript Avançado/Atividade Pratica - Aula 3 - Try, Catch, Finally/tryCatch 2.js
+++ JavaScript/JavaScript Avançado/Atividade Pratica - Aula 3 - Try, Catch, Finally/tryCatch 2.js
@@ -1,3 +1,13 @@
+/*Criar um tratamento de erro que tenha 3 consoles.
+
+● Primeiro ao entrar no escopo principal do tratamento de erro colocar um
+console.log com o texto “Iniciando o processamento”;
+
+● Segundo caso ocorra erro colocar um console.error com o erro que ocorreu.
+
+● Terceiro no final da execução do tratamento colocar um console.log com o texto
+“Fim do processo” */
+
 try {
   console.log("Iniciando processamento.");
 } catch (error) {
--- JavaScript/JavaScript Avançado/Atividade Pratica - Aula 3 - Try, Catch, Finally/tryCatch.js
+++ JavaScript/JavaScript Avançado/Atividade Pratica - Aula 3 - Try, Catch, Finally/tryCatch.js
@@ -1,3 +1,15 @@
+/*
+Crie um código de tratamento de erro onde possua 3 níveis de tratamento de erro um dentro do outro. Nos níveis mais internos ao cair no catch, deve ser forçado outro erro para que ele caia no catch do pai.
+
+
+● No try do nível mais baixo (mais interno), forçar uma exceção.
+
+● Nos catchs dos níveis internos deve ser feito um console.log e após forçar o erro para o pai.
+
+● O comportamento acima deve ser feito até que chegue no nível mais alto ou mais externo. 
+
+● Somente no nível mais alto ou mais externo será feito um console.error do erro gerado sem forçar outro erro. */
+
 try {
   try {
     try {
